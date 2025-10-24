f8c32df14ae76dff405a9c10ebe9d8d3ad917303
Raisul Islam <shihasraisul12@gmail.com>
2025-10-24 23:36:43 +0600

    Create 01.(array) input and print array elements.c

---
+++ 01.(array) input and print array elements.c
@@ -0,0 +1,16 @@
+                 #include<stdio.h>
+                 int main(){
+     int A;
+     printf("Enter size of Array : ");
+     scanf("%d",&A);
+     int arr[A],i;
+     printf("Enter elements of array: ");
+     for(i=0;i<A;i++){
+     scanf("%d",&arr[i]);
+     }
+     printf("Array is : ");
+     for(i=0;i<N; i++){
+     printf("%d ",arr[i]);
+     }
+ return 0;
+  }
